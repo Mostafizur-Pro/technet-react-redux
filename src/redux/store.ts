06c94690a1bdd60bad0f05f462087d6b758df513
Mostafizur-pro <mostafizur0195@gmail.com>
2023-07-06 23:20:51 +0600

    cart slice and handling add product to redux

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -1,6 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import cartReducer from './feature/cart/cartSlice';
+
 const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartReducer,
+  },
 });
 
 export type RootState = ReturnType<typeof store.getState>;
