5156d20b9c7a36082cec58e9ce8b75f529893861
Mostafizur-pro <mostafizur0195@gmail.com>
2023-07-07 22:56:50 +0600

    product quantity increate and descrease and removed

--- src/components/Cart.tsx
+++ src/components/Cart.tsx
@@ -13,11 +13,16 @@ import {
 } from 'react-icons/hi';
 import { Button } from './ui/button';
 
-import { useAppSelector } from '@/redux/hook';
+import { useAppDispatch, useAppSelector } from '@/redux/hook';
+import {
+  addToCart,
+  removeFromCart,
+  removeOne,
+} from '@/redux/feature/cart/cartSlice';
 
 export default function Cart() {
   const { products } = useAppSelector((state) => state.cart);
-
+  const dispatch = useAppDispatch();
   //! Dummy data
 
   // const products: IProduct[] = [];
@@ -55,13 +60,14 @@ export default function Cart() {
                 </p>
               </div>
               <div className="border-l pl-5 flex flex-col justify-between">
-                <Button>
+                <Button onClick={() => dispatch(addToCart(product))}>
                   <HiOutlinePlus size="20" />
                 </Button>
-                <Button>
+                <Button onClick={() => dispatch(removeOne(product))}>
                   <HiMinus size="20" />
                 </Button>
                 <Button
+                  onClick={() => dispatch(removeFromCart(product))}
                   variant="destructive"
                   className="bg-red-500 hover:bg-red-400"
                 >
